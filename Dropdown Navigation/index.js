d2543759b6c5d5ddcc61c6dc96fbcd7e395a9fbe
sara-mansori <sara.mansori.amar@gmail.com>
2021-05-01 10:34:39 +0200

    close function
    function that makes the navbar hide when anything other than the arrow is clicked

--- Dropdown Navigation/index.js
+++ Dropdown Navigation/index.js
@@ -1,10 +1,21 @@
 const nav = document.getElementById("nav");
 const openArrow = document.getElementById("open-nav");
 const closeArrow = document.getElementById("close-nav");
+const container = document.getElementById("container");
 
 openArrow.addEventListener("click", toggle);
 closeArrow.addEventListener("click", toggle);
 
 function toggle() {
     nav.classList.toggle("active");
+}
+
+container.addEventListener("click", close);
+
+function close(event) {
+    console.log(event.target.classList);
+    if (!event.target.classList.contains("material-icons-outlined")) {
+        console.log("remove");
+        nav.classList.remove("active");
+    }
 }
