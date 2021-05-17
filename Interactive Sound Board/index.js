5dbb710d56cd61897bfe6d9a4e799cc9a20373ad
sara-mansori <sara.mansori.amar@gmail.com>
2021-05-17 14:47:26 +0200

    changing audio names

--- Interactive Sound Board/index.js
+++ Interactive Sound Board/index.js
@@ -1,2 +1,10 @@
-const meowButton = document.getElementById("meow");
-console.log(meowButton);
+const buttons = document.querySelectorAll("button");
+console.log(buttons);
+
+meowButton.addEventListener("click", () => playSound(meow));
+
+function playSound(button) {
+    const id = button.id;
+    const sound = new Audio(`./sounds./${id}.wav`);
+    sound.play();
+}
