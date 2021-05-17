f52afe0ebf71b7dfb29c30ddcd88f8961807134d
sara-mansori <sara.mansori.amar@gmail.com>
2021-05-17 15:30:15 +0200

    stopPlaying function added
    stops the audio when another button is pushed

--- Interactive Sound Board/index.js
+++ Interactive Sound Board/index.js
@@ -1,9 +1,21 @@
 const buttons = document.querySelectorAll("button");
+let current = "";
 
 for (let button of buttons) {
     button.addEventListener("click", playSound);
 }
 
+function stopPlaying() {
+    if (current !== "") {
+        current.pause();
+    }
+}
+
 function playSound(button) {
-    new Audio(`./sounds./${button.target.id}.wav`).play();
+    stopPlaying();
+
+    let audio = new Audio(`./sounds./${button.target.id}.wav`);
+    current = audio;
+    audio.currentTime = 0;
+    audio.play();
 }
