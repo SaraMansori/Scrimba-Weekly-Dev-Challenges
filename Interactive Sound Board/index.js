92795894edb71e9e9f830b903b30d858195e36ce
sara-mansori <sara.mansori.amar@gmail.com>
2021-05-17 14:58:04 +0200

    playSound function finished

--- Interactive Sound Board/index.js
+++ Interactive Sound Board/index.js
@@ -1,10 +1,9 @@
 const buttons = document.querySelectorAll("button");
-console.log(buttons);
 
-meowButton.addEventListener("click", () => playSound(meow));
+for (let button of buttons) {
+    button.addEventListener("click", playSound);
+}
 
 function playSound(button) {
-    const id = button.id;
-    const sound = new Audio(`./sounds./${id}.wav`);
-    sound.play();
+    new Audio(`./sounds./${button.target.id}.wav`).play();
 }
