16e3ded01df05a91ff33a32acc2996ae5a82ddcf
sara-mansori <sara.mansori.amar@gmail.com>
2021-05-30 20:49:37 +0200

    spin animation added

--- Hungry Cat/index.js
+++ Hungry Cat/index.js
@@ -1,11 +1,17 @@
 const fish = document.getElementById("fish");
 const cat = document.getElementById("cat");
 
-fish.addEventListener("click", feedCat, false);
+fish.addEventListener("click", feedCat);
 
 function feedCat() {
     cat.classList.remove("hungry");
+    cat.classList.add("fed");
+
     setTimeout(function () {
         cat.classList.add("hungry");
     }, 100);
+
+    setTimeout(function () {
+        cat.classList.remove("fed");
+    }, 2000);
 }
